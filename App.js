5a3ebcc13493e73ccbfcb172ce5e2f1523dec847
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-03-16 21:17:17 +0600

    TextInput File and pops add

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const handleInputChange = text => {
+    alert(text);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>I Love My Father and Mother and My Sister!!</Text>
-      <Text>My Name is Sajib</Text>
+      <Text>Hello World!</Text>
+      <Text>My First React Native App</Text>
+      <TextInput
+        style={{ height: 30, borderColor: 'green', borderWidth: 4 }}
+        placeholder="I am placeholder Can't Love me!"
+        onChangeText={text => handleInputChange(text)}
+      />
       <StatusBar style="auto" />
     </View>
   );
