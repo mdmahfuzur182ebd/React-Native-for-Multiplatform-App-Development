e4f4c50860695293e5679bb69751b51c95945cb2
Mahfuzur <mdmahfuzur7788@gmail.com>
2022-03-16 20:52:52 +0600

    First comment

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>I Love My Father and Mother and My Sister!!</Text>
+      <Text>My Name is Sajib</Text>
       <StatusBar style="auto" />
     </View>
   );
